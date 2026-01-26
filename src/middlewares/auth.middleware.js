90622bd55fceb5209063fde28eb16e478ebba589
anas20023 <anasibnebelal400@gmail.com>
2026-01-26 19:15:10 +0600

    Fixes in the Auth

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -2,12 +2,12 @@ import jwt from 'jsonwebtoken';
 
 const verifyToken = (req, res, next) => {
     const token =
-        req.cookies.token || req.body.token || req.query.token || req.headers['x-access-token'] || req.headers['authorization'];
+        req.cookies.token || req.headers['authorization'];
 
     if (!token) {
         return res.status(403).json({ message: 'A token is required for authentication' });
     }
-
+   // console.log(token)
     try {
         const bearer = token.startsWith('Bearer ') ? token.slice(7, token.length) : token;
         const decoded = jwt.verify(bearer, process.env.JWT_SECRET || 'your_jwt_secret_key'); // Fallback for dev
