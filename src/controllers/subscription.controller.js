f2486f95823b8a0b6bef51a7a02ba1e541ae3151
anas20023 <anasibnebelal400@gmail.com>
2026-01-29 07:27:54 +0600

    Some refactoring

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -1,18 +1,17 @@
-import e from "express";
 import * as subscriptionService from "../services/subscription.service.js";
-export const createSubscription=async(req,res)=>{
-    const data= req.body
-    if(req.user.role!=="admin" || req.user.role!=="mod"){
-       return res.status(401).json({
-            message:"You are not authorized for this Operation"
+export const createSubscription = async (req, res) => {
+    const data = req.body
+    if (req.user.role !== "admin" || req.user.role !== "mod") {
+        return res.status(401).json({
+            message: "You are not authorized for this Operation"
         })
     }
     try {
-        const umm= await  subscriptionService.createSubscription(data)
-       return res.status(201).json(umm)
+        const yeapapa_umm = await subscriptionService.createSubscription(data)
+        return res.status(201).json(yeapapa_umm)
     } catch (e) {
-       return res.status(400).json({
-            message:e.message
+        return res.status(400).json({
+            message: e.message
         })
     }
 }
