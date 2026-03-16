d5a627768032700254940fc96971fc4cb3f2b9fc
anas20023 <anasibnebelal400@gmail.com>
2026-03-17 05:36:58 +0600

    Revert maxAge option in AUTH_COOKIE_OPTIONS to 30 days

--- src/config/auth.config.js
+++ src/config/auth.config.js
@@ -5,6 +5,6 @@ export const AUTH_COOKIE_OPTIONS = {
     secure: true,          // REQUIRED for SameSite=None
     sameSite: 'none',
     path: '/',
-    // maxAge: 1000 * 60 * 60 * 24 * 30 // 30 days
-    maxAge: 1000 * 60 * 2
+    maxAge: 1000 * 60 * 60 * 24 * 30 // 30 days
+    // maxAge: 1000 * 60 * 2
 };
