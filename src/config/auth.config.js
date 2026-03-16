805b7a33e531a847045c9e5f2714d1b97a3c62eb
anas20023 <anasibnebelal400@gmail.com>
2026-03-17 04:00:00 +0600

    temporary cookie secure false for testing

--- src/config/auth.config.js
+++ src/config/auth.config.js
@@ -2,8 +2,8 @@ export const AUTH_COOKIE_NAME = 'auth_token';
 
 export const AUTH_COOKIE_OPTIONS = {
     httpOnly: true,
-    secure: true,          // REQUIRED for SameSite=None
-    sameSite: 'strict',
+    secure: false,          // REQUIRED for SameSite=None
+    sameSite: 'none',
     path: '/',
     maxAge: 1000 * 60 * 60 * 24 * 30 // 30 days
 };
