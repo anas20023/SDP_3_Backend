cf2f3a436ad3ed68c0483db4888866fea0ef821b
anas20023 <anasibnebelal400@gmail.com>
2026-03-17 04:13:24 +0600

    fixed - "no changes in Backend !"

--- src/config/auth.config.js
+++ src/config/auth.config.js
@@ -2,7 +2,7 @@ export const AUTH_COOKIE_NAME = 'auth_token';
 
 export const AUTH_COOKIE_OPTIONS = {
     httpOnly: true,
-    secure: false,          // REQUIRED for SameSite=None
+    secure: true,          // REQUIRED for SameSite=None
     sameSite: 'none',
     path: '/',
     maxAge: 1000 * 60 * 60 * 24 * 30 // 30 days
