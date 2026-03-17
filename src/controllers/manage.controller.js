fee29b9780a84de9a979530a44d69ec0ed3da41b
anas20023 <anasibnebelal400@gmail.com>
2026-03-17 23:01:18 +0600

    Server Side Refactoring done

--- src/controllers/manage.controller.js
+++ src/controllers/manage.controller.js
@@ -12,9 +12,10 @@ export const getusers= async(req,res)=>{
 }
 export const getanalytics=async (req,res)=>{
     try {
-        const userAnalytics=await manageService.getLast30DaysAnalytics()
+        const userAnalytics=await manageService.getAnalytics()
+        const suggestionAnalysis= await manageService.getsuggestionAnalysis()
         res.set("Cache-Control", "public, max-age=60")
-        return res.status(200).json(userAnalytics)
+        return res.status(200).json({userAnalytics,suggestionAnalysis})
     } catch (error) {
         return res.status(404).json({
             message:"No Analytics Available"
