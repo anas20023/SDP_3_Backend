8dbca536138bd121017d0599b5d5895a663a7fee
anas20023 <anasibnebelal400@gmail.com>
2026-03-17 21:16:54 +0600

    user Analytics done git add .

--- src/services/manage.service.js
+++ src/services/manage.service.js
@@ -1,6 +1,6 @@
 import NodeCache from "node-cache"
 import Users from "../model/users.js"
-const cache = new NodeCache({ stdTTL: 60 }) 
+const cache = new NodeCache({ stdTTL: 60 })
 export const getusers = async () => {
     const cachedUsers = cache.get("users")
     if (cachedUsers) {
@@ -17,31 +17,34 @@ export const getLast30DaysAnalytics = async () => {
     }
     const today = new Date()
     today.setHours(0, 0, 0, 0)
-    
+
     const thirtyDaysAgo = new Date(today)
     thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30)
-    
+
     const users = await Users.find({ createdAt: { $gte: thirtyDaysAgo } }).select('-passwordHash')
-    
+
     const analytics = Array.from({ length: 32 }, (_, i) => {
         const dayStart = new Date(thirtyDaysAgo)
         dayStart.setDate(dayStart.getDate() + i)
         dayStart.setHours(0, 0, 0, 0)
-        
+
         const dayEnd = new Date(dayStart)
         dayEnd.setHours(23, 59, 59, 999)
-        
-        const dayUsers = users.filter(u => u.createdAt >= dayStart && u.createdAt <= dayEnd)
-        
+
+        const cdayUsers = users.filter(u => u.createdAt >= dayStart && u.createdAt <= dayEnd)
+        const udayUsers = users.filter(u => u.updatedAt >= dayStart && u.updatedAt <= dayEnd)
+
         return {
             id: i + 1,
-            name:[...dayUsers],
-            count: dayUsers.length,
+            cc: cdayUsers.length,
+            uc: udayUsers.length,
             "created_at": dayStart,
-            day:  dayStart.toISOString().split('T')[0]
+            day: dayStart.toISOString().split('T')[0]
+            ,
+            "display_date": dayStart.toLocaleDateString('en-US', { day: '2-digit', month: 'short' })
         }
     })
-    
+
     cache.set("last_30_days", analytics)
     return analytics
 }
