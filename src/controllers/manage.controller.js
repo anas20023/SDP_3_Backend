3ff53a3d23ae4c3303c56481d1defbbda14cecf5
anas20023 <anasibnebelal400@gmail.com>
2026-04-07 00:16:35 +0600

    feat: implement manage controller for user and suggestion administration

--- src/controllers/manage.controller.js
+++ src/controllers/manage.controller.js
@@ -2,7 +2,7 @@ import * as manageService from '../services/manage.service.js'
 export const getusers= async(req,res)=>{
     try {
         const users= await manageService.getusers()
-        res.set("Cache-Control", "public, max-age=60")
+       // res.set("Cache-Control", "public, max-age=60")
         return res.status(200).json(users)
     } catch (error) {
        return res.status(404).json({
@@ -16,7 +16,7 @@ export const getanalytics=async (req,res)=>{
         const suggestionAnalysis= await manageService.getsuggestionAnalysis()
         const starAnalysis=await manageService.getstarAnalysis()
        // console.log(starAnalysis)
-        res.set("Cache-Control", "public, max-age=10")
+        //res.set("Cache-Control", "public, max-age=10")
         return res.status(200).json({userAnalytics,suggestionAnalysis,starAnalysis})
     } catch (error) {
         return res.status(404).json({
